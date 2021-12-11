b4024bc1255b3736215e1bdc4a4117773f1837ae
Dan Zhao <dan.zhao@mygeorgian.ca>
2021-12-11 17:30:00 -0500

    改变了 environment.prod.ts里面的那个apiServer,不能有最后那个竖杠

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./controllers/index');
 var usersRouter = require('./controllers/users');
-var artistsRouter = require('./controllers/artists');
+const artistsController = require('./controllers/artists')
 
 var app = express();
-//新加的1
-//globals 里面加入了 passport config（github）那个，所以要放前面
+
+// global vars for various uses (db connection / passport config)
 const globals = require('./config/globals')
 
 // view engine setup
@@ -25,57 +25,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/artists',artistsRouter)
+app.use('/api/artists', artistsController)
 
-//新加的2
 // mongodb connection w/mongoose
 const mongoose = require('mongoose')
 
-
-//这里是dotenv的信息，
-// if in dev mode, use our .env file for config variables
-if(process.env.NODE_ENV !== 'production'){
+// if in dev mode, use our .env file for config vars
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-
-
-
-//这里如果是测试，是不是能连接上服务器
-mongoose.connect(process.env.DATABASE_URL,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(
     (res) => {
-      console.log('Connected to MongoDB')
+        console.log('Connected to MongoDB')
     }
 ).catch(() => {
-  console.log('Could not connect to MongoDB')
+    console.log('Could not connect to MongoDB')
 })
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-
 // start server here rather than in bin/www
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Running on port ${port}`))
-
+//const port = process.env.PORT || 3000
+//app.listen(port, () => console.log(`Running on port ${port}`))
 
 module.exports = app;
